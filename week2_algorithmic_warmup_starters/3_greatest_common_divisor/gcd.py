b0b1474b7fdb324e041b7e835d3965470c6af353
rekenit <ntabenism@gmail.com>
2018-03-14 17:05:46 +0200

    challenge 4

--- week2_algorithmic_warmup_starters/3_greatest_common_divisor/gcd.py
+++ week2_algorithmic_warmup_starters/3_greatest_common_divisor/gcd.py
@@ -1,5 +1,16 @@
 # Uses python3
+""" 
+Problem Description:
+....................
+Task.           Given two integers a and b, find their greatest common divisor
+Input Format.   the two integers a , b are given in same line seperated by space
+Constraints.    1 <= a, b <= 2*10exp9
+Output Format.  Output GCD(a, b)
+
+"""
+
 import sys
+
 
 def gcd_naive(a, b):
     current_gcd = 1
@@ -7,10 +18,16 @@ def gcd_naive(a, b):
         if a % d == 0 and b % d == 0:
             if d > current_gcd:
                 current_gcd = d
-
     return current_gcd
+
+def gcd_euclid(a, b):
+    if b == 0:
+        return a
+    a = a%b
+    return gcd_euclid(b, a)
+    
 
 if __name__ == "__main__":
     input = sys.stdin.read()
     a, b = map(int, input.split())
-    print(gcd_naive(a, b))
+    print(gcd_euclid(a, b))
