edd3698b064a690cae71f47f6af70518a14d571b
saebs <ntabenism@gmail.com>
2020-11-01 00:30:59 +0200

    bug fix - was not including last number

--- week2_algorithmic_warmup_starters/1_fibonacci_number/fibonacci.rs
+++ week2_algorithmic_warmup_starters/1_fibonacci_number/fibonacci.rs
@@ -3,11 +3,11 @@
 use std::env;
 
 /// Naive Recursive Method
-fn fib(n: i64) -> i64 {
+fn fib_naive(n: i64) -> i64 {
 	match n {
 		0i64 => 0i64,
 		1i64 => 1i64,
-		_ => fib(n - 1) + fib(n -2),
+		_ => fib_naive(n - 1) + fib_naive(n -2),
     }
 }
 
@@ -20,19 +20,19 @@ fn fib_optimised(n: usize) -> usize {
 		0 => 0,
 		1 => 1,
 		_ => {
-				for i in 2..n {
+				for i in 2..n+1 {
 					f.push(f[i -1] + f[i -2]);
 				}
-				f[n-1]
+				f[n]
 		}
 	}
 }
 fn main() {
-	assert_eq!(fib(0), 0);
-	assert_eq!(fib(1), 1);
+	assert_eq!(env::args().len(), 2);
+	assert_eq!(fib_naive(0), 0);
+	assert_eq!(fib_naive(1), 1);
 	assert_eq!(fib_optimised(0), 0);
 	assert_eq!(fib_optimised(1), 1);
-	assert_eq!(env::args().len(), 2);
 	let n: Vec<String> = env::args().skip(1).collect();
 	let n = fib_optimised(n[0][..].parse::<usize>().unwrap());
 	println!("{}", n);
