6bd74e2e85a7b60d6a12b3855a106504f41774e5
saebs <ntabenism@gmail.com>
2020-12-01 16:31:06 +0200

    use rustc instead

--- algs/run_grader.sh
+++ algs/run_grader.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo 1. aplusb
 echo Enter Name of Programming Challenge
 read -p 'solution: ' appName
 testData=test_case.txt
@@ -7,13 +8,13 @@ memLimit=524288
 cd grader_report 
 rm *.txt
 cd ..
-# /usr/bin/time -f'\nUser\tSystem\tReal\tMemory\n%Us\t%Ss\t%es\t%Mkb\n' -o "perfomance_tests_logs//$appName.log" cargo run --bin $appName $testData
-/usr/bin/time -f'\nUser(s)\tSystem(s)\tMemory(kb)\n%U\t%S\t\t%M\n' -o "grader_report//$appName.txt" cargo run --bin $appName $testData
+rustc src/$appName.rs -o bin/$appName 
+/usr/bin/time -f'\nUser(s)\tSystem(s)\tMemory(kb)\n%U\t%S\t\t%M\n' -o "grader_report//$appName.txt"  bin/$appName  
 echo
 echo CONSTRAINTS
 echo Time Limit: $timeLimit s
 echo Memory Limit $memLimit kb 
 echo 
 echo BENCHMARKS
-cat perfomance_tests_logs/$appName.log
+cat grader_report/$appName.txt
 # cat /dev/stdin | cut -d' ' -f 2,3 | sort
---
+++ algs/src/aplusb.rs
@@ -0,0 +1,35 @@
+//! Week 1 
+//! APlusB
+// Sum of Two Digits Problem
+// Compute the sum of two single digit numbers.
+// Input: Two single digit numbers.
+// Output: The sum of these numbers.
+// 2 + 3 = 5
+// We start from this ridiculously simple problem to show you the
+// pipeline of reading the problem statement, designing an algorithm, implementing
+// it, testing and debugging your program, and submitting it to
+// the grading system.
+// Input format. Integers a and b on the same line (separated by a space).
+// Output format. The sum of a and b.
+// Constraints. 0  a;b  9.
+// Sample.
+// Input:
+// 9 7
+// Output:
+// 16
+// Time limits (sec.):
+// C C++ Java Python Haskell JavaScript Scala
+// 1 1 1.5 5 2 5 3
+// Memory limit. 512 Mb.
+
+fn main() -> std::io::Result<()> {
+    let mut buff = String::new();
+    ::std::io::stdin().read_line(&mut buff)?;
+    let mut words = buff.split_whitespace();
+
+    let a: i64 = words.next().unwrap().parse().unwrap();
+    let b: i64 = words.next().unwrap().parse().unwrap();
+
+    println!("{}", a + b);
+    Ok(())
+}
