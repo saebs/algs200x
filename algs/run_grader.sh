d50eb3c078449fec1a82cec5502a6b89a75b3eef
saebs <ntabenism@gmail.com>
2020-12-01 13:31:41 +0200

    cleaner ouput

--- algs/run_grader.sh
+++ algs/run_grader.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-week=week1
+echo Enter Name of Programming Challenge
 read -p 'solution: ' appName
 testData=test_case.txt
 timeLimit=1
 memLimit=524288
 rm  "perfomance_tests_logs//$appName.log"
-/usr/bin/time -f'\nUser\tSystem\tReal\tMemory\n%Us\t%Ss\t%es\t%Mkb\n' -o "perfomance_tests_logs//$appName.log" cargo run --bin $appName $testData
+# /usr/bin/time -f'\nUser\tSystem\tReal\tMemory\n%Us\t%Ss\t%es\t%Mkb\n' -o "perfomance_tests_logs//$appName.log" cargo run --bin $appName $testData
+/usr/bin/time -f'\nUser\tSystem\tMemory\n%Us\t%Ss\t%Mkb\n' -o "perfomance_tests_logs//$appName.log" cargo run --bin $appName $testData
 echo
 echo CONSTRAINTS
 echo Time Limit: $timeLimit s
