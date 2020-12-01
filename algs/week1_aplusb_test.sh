f3a0844279ffc4d6c2d06da21154ce0898ee191d
saebs <ntabenism@gmail.com>
2020-12-01 13:22:59 +0200

    simplify

--- algs/week1_aplusb_test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-appName=week1_aplusb
-cargo test ey  
--- algs/wk1_maximum_pairwise_product.sh
+++
@@ -1,2 +0,0 @@
-#! bash
-/usr/bin/time -o perfomance_tests_logs/maximum_pair_wise.log cargo run --bin  week1 maximumpairwise maximum_pair_wise_test_case.txt
