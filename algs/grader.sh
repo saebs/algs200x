05daa3d7837b509ffce33cfb85a0c0429abc8fe2
saebs <ntabenism@gmail.com>
2020-12-01 17:29:24 +0200

    grader tool v0.1

--- algs/grader.sh
+++ algs/grader.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+#TODO SOME error handling when you have time
 solutionSourceCode="$1"
 echo $solutionSourceCode
 timeLimit=1
 memLimit=524288
-cd grader_report 
+cd report 
 rm *.txt
 cd ..
 # strip rust extension and parent folder
@@ -13,11 +14,11 @@ app=${app:4}
 # Compile Solution
 rustc $solutionSourceCode -o bin/$app 
 # Measure Runtime 
-/usr/bin/time -f'\nUser(s)\tSystem(s)\tMemory(kb)\n%U\t%S\t\t%M\n' -o "grader_report//$app.txt"  bin/$app  
+/usr/bin/time -f'\nUser(s)\tSystem(s)\tMemory(kb)\n%U\t%S\t\t%M\n' -o "report/$app.txt"  bin/$app  
 echo
 echo CONSTRAINTS
 echo Time Limit: $timeLimit s
 echo Memory Limit $memLimit kb 
 echo 
 echo BENCHMARKS
-cat grader_report/$app.txt
+cat report/$app.txt
