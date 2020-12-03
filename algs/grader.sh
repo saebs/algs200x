a1448ca3839be9460e39fb601f4e90b587ebb219
saebs <ntabenism@gmail.com>
2020-12-03 17:18:33 +0200

    kiss

--- algs/grader.sh
+++ algs/grader.sh
@@ -4,9 +4,6 @@ solutionSourceCode="$1"
 echo $solutionSourceCode
 timeLimit=1
 memLimit=524288
-cd report 
-rm *.txt
-cd ..
 # strip rust extension and parent folder
 app=${solutionSourceCode%.rs}
 app=${app:4}
@@ -14,11 +11,5 @@ app=${app:4}
 # Compile Solution
 rustc $solutionSourceCode -o bin/$app 
 # Measure Runtime 
-/usr/bin/time -f'\nUser(s)\tSystem(s)\tMemory(kb)\n%U\t%S\t\t%M\n' -o "report/$app.txt"  bin/$app  
-echo
-echo CONSTRAINTS
-echo Time Limit: $timeLimit s
-echo Memory Limit $memLimit kb 
-echo 
-echo BENCHMARKS
-cat report/$app.txt
+# /usr/bin/time -f'\nUser(s)\tSystem(s)\tMemory(kb)\n%U\t%S\t\t%M\n' -o "report/$app.txt"  bin/$app  
+time -p bin/$app
