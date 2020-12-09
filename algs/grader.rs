99088600b4dbde4655ffc81ba509b5dfa45d5df9
saebs <ntabenism@gmail.com>
2020-12-09 19:33:26 +0200

    lean up

--- algs/grader.rs
+++ algs/grader.rs
@@ -1,4 +1,4 @@
-//! Self Grading Tool 
+//! Self Grading Tool
 
 // MEASURE and enforce Constraints
 // Runtime
@@ -7,15 +7,13 @@
 // TEST
 // Algorithm correctness
 
-
+use std::env;
 use std::process;
 use std::process::{Command, Stdio};
-use std::env;
 use std::time::Instant;
 // use std::fs;
 use std::str::from_utf8;
 fn main() -> std::io::Result<()> {
-
     let args: Vec<String> = env::args().skip(1).collect();
     // println!("program to grade: {}", &args[0]);
     let solution = &args[0][..];
@@ -33,24 +31,29 @@ fn main() -> std::io::Result<()> {
     }
     println!("solution name: {}\n", &app);
     // build solution
-    let mut build = Command::new("./build.sh").arg(&solution).spawn();
-                // remember to kill
-    if let Ok(mut out) = build {
-        out.wait(); 
-    }
+    let mut build = Command::new("./build.sh")
+        .arg(&solution)
+        .spawn()
+        .unwrap()
+        .wait()
+        .expect("ma 1");
+    // remember to kill
+    // if let Ok(mut out) = build {
+    //     out.wait();
+    // }
     let now = Instant::now();
     // solution spawned here
     // record / capture process id
     // run many tests soon as well
     let elapsed = now.elapsed();
 
-// ***************************************************
-// IMPLENTING THE BASH SCRIPT  
-// to extract the memory used by a specified process id e.g.
-//  
-// $ pmap 6860 | tail -n 1 | awk 'END {print $NF}'
-//
-//****************************************************
+    // ***************************************************
+    // IMPLENTING THE BASH SCRIPT
+    // to extract the memory used by a specified process id e.g.
+    //
+    // $ pmap 6860 | tail -n 1 | awk 'END {print $NF}'
+    //
+    //****************************************************
 
     let proc_id = process::id().to_string();
     // pmap ID ...
@@ -59,7 +62,7 @@ fn main() -> std::io::Result<()> {
         .stdout(Stdio::piped())
         .spawn()
         .expect("failed to start");
-    
+
     let pmap_out = pmap.stdout.expect("Failed bafo");
 
     // ... | tail -n 1
@@ -69,9 +72,9 @@ fn main() -> std::io::Result<()> {
         .stdout(Stdio::piped())
         .spawn()
         .expect("Failed to start tail part");
-    
+
     let tail_out = tail.stdout.expect("kwehlule");
-// ... | awk 'END {print $NF}'
+    // ... | awk 'END {print $NF}'
     let awk = Command::new("awk")
         .arg("END {print $NF}")
         .stdin(Stdio::from(tail_out))
@@ -80,7 +83,7 @@ fn main() -> std::io::Result<()> {
         .expect("Failed to start  awk part");
 
     let awk_out = awk.wait_with_output().expect("awk part fail");
-    let mut mem_used = String::new(); 
+    let mut mem_used = String::new();
     match std::str::from_utf8(awk_out.stdout.as_slice()) {
         Ok(memo) => mem_used = memo.to_string(),
         Err(_) => (),
@@ -91,5 +94,4 @@ fn main() -> std::io::Result<()> {
     println!("memory: {}", &mem_used);
     println!("Parent pid is {}", process::id());
     Ok(())
-
 }
