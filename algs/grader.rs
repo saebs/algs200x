5e53d4190a96a18f851842972893b48697ecab1c
saebs <ntabenism@gmail.com>
2020-12-09 20:32:55 +0200

    recording memory of solution

--- algs/grader.rs
+++ algs/grader.rs
@@ -37,16 +37,25 @@ fn main() -> std::io::Result<()> {
         .unwrap()
         .wait()
         .expect("ma 1");
-    // remember to kill
-    // if let Ok(mut out) = build {
-    //     out.wait();
-    // }
+
     let now = Instant::now();
     // solution spawned here
+    // let solution_child = Command::new("bin/./".to_owned() + &app )
+    //     .spawn()
+    //     .unwrap()
+    //     .wait()
+    //     .expect("atanga");
+    
+    let mut solution_child = Command::new("bin/./".to_owned() + &app )
+        .spawn()
+        .unwrap();
     // record / capture process id
+    println!("{:?}", solution_child);
     // run many tests soon as well
+    // let proc_id = process::id().to_string();
+    let proc_id = solution_child.id().to_string();
     let elapsed = now.elapsed();
-
+    
     // ***************************************************
     // IMPLENTING THE BASH SCRIPT
     // to extract the memory used by a specified process id e.g.
@@ -54,44 +63,47 @@ fn main() -> std::io::Result<()> {
     // $ pmap 6860 | tail -n 1 | awk 'END {print $NF}'
     //
     //****************************************************
-
-    let proc_id = process::id().to_string();
+    
     // pmap ID ...
     let pmap = Command::new("pmap")
-        .arg(proc_id)
-        .stdout(Stdio::piped())
-        .spawn()
-        .expect("failed to start");
-
+    .arg(&proc_id)
+    .stdout(Stdio::piped())
+    .spawn()
+    .expect("failed to start");
+    
     let pmap_out = pmap.stdout.expect("Failed bafo");
-
+    
     // ... | tail -n 1
     let tail = Command::new("tail")
-        .arg("-n 1")
-        .stdin(Stdio::from(pmap_out))
-        .stdout(Stdio::piped())
-        .spawn()
-        .expect("Failed to start tail part");
-
+    .arg("-n 1")
+    .stdin(Stdio::from(pmap_out))
+    .stdout(Stdio::piped())
+    .spawn()
+    .expect("Failed to start tail part");
+    
     let tail_out = tail.stdout.expect("kwehlule");
     // ... | awk 'END {print $NF}'
     let awk = Command::new("awk")
-        .arg("END {print $NF}")
-        .stdin(Stdio::from(tail_out))
-        .stdout(Stdio::piped())
-        .spawn()
-        .expect("Failed to start  awk part");
-
+    .arg("END {print $NF}")
+    .stdin(Stdio::from(tail_out))
+    .stdout(Stdio::piped())
+    .spawn()
+    .expect("Failed to start  awk part");
+    
     let awk_out = awk.wait_with_output().expect("awk part fail");
-    let mut mem_used = String::new();
+    let mut memory_used = String::new();
     match std::str::from_utf8(awk_out.stdout.as_slice()) {
-        Ok(memo) => mem_used = memo.to_string(),
+        Ok(ram) => memory_used = ram.to_string(),
         Err(_) => (),
     }
 
+    // process can now chill
+    solution_child.wait();
+    
     // TODO or save to file
     println!("\ntime: {:.2?}", elapsed);
-    println!("memory: {}", &mem_used);
-    println!("Parent pid is {}", process::id());
+    println!("memory: {}", &memory_used);
+    println!("id is {}", &proc_id);
+    println!("id is {}", process::id());
     Ok(())
 }
