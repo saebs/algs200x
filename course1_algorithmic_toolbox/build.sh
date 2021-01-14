8aa10d3113e389d9d61e256fe48da9ef10114034
saebs <ntabenism@gmail.com>
2021-01-14 09:58:49 +0200

    one build script

--- course1_algorithmic_toolbox/build.sh
+++ course1_algorithmic_toolbox/build.sh
@@ -9,8 +9,10 @@ app=${app:4}
 
 # Compile Solution
 echo compiling ...
-rustc --crate-type=rlib "utils.rs" --edition 2018
-rustc -Ov $solutionSourceCode -o bin/$app --extern crate=libutils.rlib -L "." --edition 2018 
+rustc "algs_std.rs" --edition 2018 \
+
+rustc -Ov $solutionSourceCode -o bin/$app --extern crate=libalgs200x.rlib -L "." --edition 2018 \
+--keep-stage 0 
 echo finished ...
 # Run Build
 # echo running ...
